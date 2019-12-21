cb5d533cd190d37b13f51ee32c83577bc77b0654
Tridib Samanta <tridibsamanta2010@gmail.com>
2019-12-21 21:03:43 +0530

    Day 00 : Hello, World.

---
+++ Day_00_Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Title   : Day_00_Solution.java
+ * Author  : Tridib Samanta
+ * Created : 21-12-2019
+ * Link    : https://www.hackerrank.com/challenges/30-hello-world/problem
+ **/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day_00_Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        scan.close();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
