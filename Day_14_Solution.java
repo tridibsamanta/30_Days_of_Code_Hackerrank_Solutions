248fa757cab8270a7f821ddf3b680b2d12b31146
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-01-06 23:27:46 +0530

    Day 14 : Scope

---
+++ Day_14_Solution.java
@@ -0,0 +1,48 @@
+/**
+ * Title   : Day_14_Solution.java
+ * Author  : Tridib Samanta
+ * Created : 06-01-2020
+ * Link    : https://www.hackerrank.com/challenges/30-scope/problem
+ **/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+     Difference (int [] elements) {
+        this.elements = elements;
+    }
+
+    void computeDifference() {
+        int max = Arrays.stream(elements).max().getAsInt();
+        int min = Arrays.stream(elements).min().getAsInt();
+        maximumDifference = max - min ;
+    }
+
+}
+
+public class Day_14_Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
