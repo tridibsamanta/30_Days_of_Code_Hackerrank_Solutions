e3161e7e16e1c648a2b37e1e02939c6c69b171ce
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-01-09 21:38:44 +0530

    Day 17 : More Exceptions

---
+++ Day_17_Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Title   : Day_17_Solution.java
+ * Author  : Tridib Samanta
+ * Created : 09-01-2020
+ * Link    : https://www.hackerrank.com/challenges/30-more-exceptions/problem
+ **/
+
+import java.util.*;
+import java.io.*;
+
+class Calculator {
+    int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
+        } else {
+            return (int) Math.pow(n, p);
+        }
+    }
+}
+
+class Day_17_Solution{
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
