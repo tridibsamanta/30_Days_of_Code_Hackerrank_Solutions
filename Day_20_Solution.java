99643f4acf5a35fd7536e6d167fb2dcdf6dd12aa
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-01-15 23:50:12 +0530

    Day 20 : Sorting

---
+++ Day_20_Solution.java
@@ -0,0 +1,54 @@
+/**
+ * Title   : Day_20_Solution.java
+ * Author  : Tridib Samanta
+ * Created : 15-01-2020
+ * Link    : https://www.hackerrank.com/challenges/30-sorting/problem
+ **/
+
+import java.util.Scanner;
+
+public class Day_20_Solution {
+    private static int swaps = 0;
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int [] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] =  scan.nextInt();
+        }
+        scan.close();
+
+        bubbleSort(array);
+
+        System.out.println("Array is sorted in " + swaps + " swaps.");
+        System.out.println("First Element: " + array[0]);
+        System.out.println("Last Element: " + array[array.length - 1]);
+    }
+
+    private static void bubbleSort(int [] array) {
+        if (array == null) {
+            return;
+        }
+        boolean swapped = true;
+        int endOffset = 0;
+        while (swapped) {
+            swapped = false;
+            for (int i = 1; i < array.length - endOffset; i++) {
+                if (array[i-1] > array[i]){
+                    swap(array, i-1, i);
+                    swapped = true;
+                }
+            }
+            endOffset++;
+        }
+    }
+
+    private static void swap(int [] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        swaps++;
+    }
+}
