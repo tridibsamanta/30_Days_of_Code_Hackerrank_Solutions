bfbea56302c37016d4d7f8ae5fc51606e3e2b831
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-01-21 15:52:12 +0530

    Day 25 : Running Time and Complexity

---
+++ Day_25_Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Title   : Day_25_Solution.java
+ * Author  : Tridib Samanta
+ * Created : 21-01-2020
+ * Link    : https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
+ **/
+
+import java.util.Scanner;
+
+public class Day_25_Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while (T-- > 0) {
+            int n = scan.nextInt();
+            System.out.println(isPrime(n) ? "Prime" : "Not prime");
+        }
+        scan.close();
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
