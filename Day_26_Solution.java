686aaf537ce41725b572d80ca6ee0f2c8381972c
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-01-22 23:52:25 +0530

    Day 26 : Nested Logic

---
+++ Day_26_Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Title   : Day_26_Solution.java
+ * Author  : Tridib Samanta
+ * Created : 22-01-2020
+ * Link    : https://www.hackerrank.com/challenges/30-nested-logic/problem
+ **/
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class Day_26_Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        LocalDate returnDate = readDate(scan);
+        LocalDate expectDate = readDate(scan);
+        scan.close();
+
+
+        int fine;
+        if (returnDate.isEqual(expectDate) || returnDate.isBefore(expectDate)) {
+            fine = 0;
+        } else if (returnDate.getMonth() == expectDate.getMonth() && returnDate.getYear() == expectDate.getYear()) {
+            fine = 15 * (returnDate.getDayOfMonth() - expectDate.getDayOfMonth());
+        } else if (returnDate.getYear() == expectDate.getYear()) {
+            fine = 500 * (returnDate.getMonthValue() - expectDate.getMonthValue());
+        } else {
+            fine = 10000;
+        }
+        System.out.println(fine);
+    }
+
+    private static LocalDate readDate(Scanner scan) {
+        int day   = scan.nextInt();
+        int month = scan.nextInt();
+        int year  = scan.nextInt();
+        return LocalDate.of(year, month, day);
+    }
+}
